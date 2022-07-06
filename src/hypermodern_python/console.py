f1c487f5a8b13ece8f2aea8ac32c34a53c00b6a8
Maxwell Volz <mvolz@fb.com>
2022-07-05 17:05:10 -0700

    locale_lang check, language selector setup

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,8 +1,9 @@
 # src/hypermodern_python/console.py
 import textwrap
-
+import locale
 import click
 import requests
+
 
 from . import __version__
 
@@ -14,12 +15,35 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 @click.version_option(version=__version__)
 def main():
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
 
-    title = data["title"]
-    extract = data["extract"]
+    # Get and set locale language
+    locale_lang, locale_encoding = locale.getdefaultlocale()
+    click.echo(textwrap.fill(locale_lang + " : " + locale_encoding))
 
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    # Language selector
+    LANG_SLICE = slice(8,10)
+
+    NEW_URL = list(API_URL)
+    NEW_URL[LANG_SLICE] = 'en'
+    NEW_URL = ''.join(NEW_URL)
+
+    click.echo(textwrap.fill(NEW_URL))
+
+
+    with requests.get(NEW_URL) as response:
+        try:
+            response.raise_for_status()
+            data = response.json()
+
+            title = data["title"]
+            extract = data["extract"]
+
+            click.secho(title, fg="green")
+            click.echo(textwrap.fill(extract))
+
+        except requests.exceptions.HTTPError as err:
+            click.secho("HTTP Error", fg="red")
+            # click.echo(textwrap.fill(err))
+            raise SystemExit(err)
+
+
